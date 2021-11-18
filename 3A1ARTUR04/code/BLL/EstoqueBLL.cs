d76950118923799c07c8309b197f599305d37a59
Artur Gonzaga <69986536+hznxg@users.noreply.github.com>
2021-11-18 08:52:56 -0300

    Add files via upload

---
+++ 3A1ARTUR04/code/BLL/EstoqueBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using _3A1ARTUR04.code.DTO;
+using _3A1ARTUR04.code.DAL;
+
+namespace _3A1ARTUR04.code.BLL
+{
+    class EstoqueBLL
+    {
+        AcessoBancoDados conexao = new AcessoBancoDados();
+        string tabela = "tbl_estoque";
+        public void Inserir (EstoqueDTO estdto)
+        {
+            string inserir = $"insert into {tabela} values(null '{estdto.Nome}','{estdto.Categoria}')";
+            conexao.ExecutarComando(inserir);
+        }
+    }
+}
---
+++ 3A1ARTUR04/code/DTO/EstoqueDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3A1ARTUR04.code.DTO
+{
+    class EstoqueDTO
+    {
+        private int id;
+        private string nome;
+        private string categoria;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+       
+    }
+}
